bce39d777ba6441bc6219feec35d3b959130bc8f
Praveen K.G <kgpraveen2904@gmail.com>
2025-06-29 13:44:23 +0530

    Switched database to PostgreSQL

--- erp_project/erp_project/settings.py
+++ erp_project/erp_project/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'erp_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'erp_db',             # name of your DB in pgAdmin
+        'USER': 'postgres',           # default superuser unless you changed it
+        'PASSWORD': 'admin123',   # the one you set during install
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
